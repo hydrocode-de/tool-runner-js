63c037d4c708b4eec5285502bf5f71063005f61f
Mirko Mälicke <mirko.maelicke@kit.edu>
2023-02-17 16:00:16 +0100

    drafting parameters implementation

---
+++ src/parameter.ts
@@ -0,0 +1,65 @@
+import * as fs from 'fs';
+import { unparse } from 'papaparse';
+
+import { ToolConfig } from "./models/ToolConfig"
+
+export const buildParameterFile = (args: any, inDir: string, toolConfig: ToolConfig): string => {
+    // build the params object
+    const params: any = {}
+
+    // iterate through all arguments
+    Object.entries(args).forEach(([paramName, paramValue]) => {
+        // get the paramName
+        const paramConfig = toolConfig.parameters[paramName];
+
+        if (paramConfig.type === 'file') {
+            // matrix
+            if (Array.isArray(paramValue) && paramValue.every(r => Array.isArray(r))) {
+                // 2D matrix
+                if (paramValue.every(row => row.every((cell: any) => typeof cell === 'number'))) {
+                    // create a dat matrix
+                    const matrix_rows = paramValue.map((row: number[]) => row.map(cell => cell.toPrecision()).join(' '))
+                    const matrix = matrix_rows.join('\r\n')
+                    
+                    // write the file and save
+                    fs.writeFileSync(`${inDir}/${paramName}.dat`, matrix)
+                    params[paramName] = `/in/${paramName}.dat`
+                }
+                // CSV
+                else if (Array.isArray(paramValue) && paramValue.every(r => typeof r === 'object')) {
+                    const csv = unparse(paramValue)
+
+                    // write
+                    fs.writeFileSync(`${inDir}/${paramName}.csv`, csv)
+                    params[paramName] = `/in/${paramName}.csv`
+                }
+            } else if (typeof paramValue === 'string') {
+                if (fs.existsSync(paramValue)) {
+                    // copy the file and save
+                    fs.copyFileSync(paramValue, `${inDir}/${paramName}.${paramValue.split('.').pop()}`)
+                    params[paramName] = `/in/${paramName}.${paramValue.split('.').pop()}`
+                } else {
+                    // plain text file is assumed
+                    fs.writeFileSync(`${inDir}/${paramName}.txt`, paramValue)
+                    params[paramName] = `/in/${paramName}.txt`
+                }
+
+            } 
+            else {
+                // just serialize it
+                fs.writeFileSync(`${inDir}/${paramName}.json`, JSON.stringify(paramValue))
+                params[paramName] = `/in/${paramName}.json`
+            }
+
+        } else {
+            params[paramName] = paramValue;
+        }    
+    })
+
+    // write the file
+    const parameters: any = {}
+    parameters[toolConfig.name] = params
+    fs.writeFileSync(`${inDir}/parameters.json`, JSON.stringify(parameters, null, 4))
+
+    return `${inDir}/parameters.json`
+}
