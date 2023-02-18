812118641f66496c662262913ebef144d55b689a
Mirko Mälicke <mirko@hydrocode.de>
2023-02-18 14:11:00 +0100

    add imports

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,6 @@
+// Run container options
 export { listTools, runTool, RunOptions } from './run';
 export { ToolConfig } from './models/ToolConfig';
+
+// api functions
+export { runServer, RunServerOptions } from './api/api'
