00d239c6f17e8d984abdd7f448706e0cafe5b6ea
Mirko Mälicke <mirko@hydrocode.de>
2023-02-18 15:48:29 +0100

    using middleware

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,25 +1,15 @@
 import * as express from 'express';
-import * as run from '../run';
-import { ToolConfig } from '../models/ToolConfig'
+import e = require('express');
+
+import { refreshCache, filterToolName, ReqTools, ReqTool } from './middleware';
+
 
 // cereate the API server
 const app = express()
 
-
-let TOOL_CACHE: ToolConfig[] = []
-
-// refresh the tool cache
-const refreshCache = async (query: {refresh?: any} = {}): Promise<void> => {
-    // check if the tools have been cached
-    if (TOOL_CACHE.length === 0 || (query.refresh && Boolean(query.refresh))) {
-        // update the tools cache
-        await run.listTools()
-            .then(tools => TOOL_CACHE = tools)
-            .then(() => console.log(`[${new Date().toISOString()}] The tools cache was updated.`))
-    }
-
-    return Promise.resolve()
-}
+// always use the caching middleware
+app.use('/tools*', refreshCache)
+app.use('/tools/:toolName*', filterToolName )
 
 
 // create the routes
@@ -38,34 +28,22 @@ app.get('/', (req, res) => {
     })
 })
 
-app.get('/tools', async (req, res) => {
-    // wait until the TOOL cache is refreshed
-    await refreshCache(req.query)    
-
+app.get('/tools', (req, res) => {
     // tools are cached now
     res.status(200).json({
-        count: TOOL_CACHE.length,
-        tools: TOOL_CACHE
+        count: (req as e.Request & ReqTools).tools.length,
+        tools: (req as e.Request & ReqTools).tools
     })
 })
 
-app.get('/tools/:toolName', async (req, res) => {
-    // wait until the TOOL cache is refreshed
-    await refreshCache(req.query)
-
-    // get the tool
-    const tool = TOOL_CACHE.find(t => t.name.toLowerCase() === req.params.toolName.toLowerCase())
-
-    if (!tool) {
-        res.status(404).json({
-            message: `The tool '${req.params.toolName}' could not be found.`
-        })
-    }
+app.get('/tools/:toolName',(req, res) => {
+    // just send back the tool
     res.status(200).json({
-        tool
+        tool: (req as e.Request<{toolName: string}> & ReqTool).tool
     })
 
 })
+
 
 export interface RunServerOptions {
     port?: number
@@ -74,6 +52,8 @@ export interface RunServerOptions {
 export const runServer = (options: RunServerOptions= {}) => {
     // get the port
     const port = options.port || process.env.PORT || 3000;
+
+
     
     // run the app
     app.listen(port, () => {
---
+++ src/api/middleware.ts
@@ -0,0 +1,58 @@
+import * as e from 'express'
+import { performance } from 'perf_hooks';
+
+import { ToolConfig } from '../models/ToolConfig'
+import * as run from '../run';
+
+
+// caching
+let TOOL_CACHE: ToolConfig[] = []
+let CACHE_AGE = performance.now()
+const MAX_AGE = process.env.CACHE_MAX_AGE || 24 * 60 * 60 * 1000
+
+export interface ReqTools {
+    tools: ToolConfig[]
+}
+
+export interface ReqTool extends ReqTools {
+    tool: ToolConfig;
+}
+
+
+// middleware to refresh the TOOL cache
+export const refreshCache = async (req: e.Request, res: e.Response, next: e.NextFunction) => {
+    // check if the tools have been cached
+    if (
+        TOOL_CACHE.length === 0 || 
+        (req.query.refresh && Boolean(req.query.refresh)) || 
+        performance.now() - CACHE_AGE > MAX_AGE)
+    {    
+        // update the tools cache
+        await run.listTools()
+            .then(tools => TOOL_CACHE = tools)
+            .then(() => console.log(`[${new Date().toISOString()}] The tools cache was updated.`))
+    }
+
+    // here the TOOL_CACHE is updated, add the list to the request
+    (req as e.Request & {tools: ToolConfig[]}).tools = TOOL_CACHE
+
+
+    // jump to the next middleware
+    next()
+}
+
+export const filterToolName = async (req: e.Request<{toolName: string}>, res: e.Response, next: e.NextFunction) => {
+    // the tools Middleware has to be run first
+    const toolName = req.params.toolName.toLowerCase()
+    const tool = (req as e.Request<{toolName: string}> & ReqTools).tools.find(t => t.name.toLowerCase() === toolName)
+
+    // check if a tool was found
+    if (tool) {
+        (req as e.Request<{toolName: string}> & ReqTool).tool = tool
+        next()
+    } else {
+        res.status(404).send({
+            message: `The tool '${toolName}' could not be found.`
+        })
+    }
+}
