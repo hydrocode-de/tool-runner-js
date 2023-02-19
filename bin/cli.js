f880c2bd5b225cf0c508b61ccf577e1d4678be3a
Mirko Mälicke <mirko@hydrocode.de>
2023-02-19 18:11:16 +0100

    update CLI

--- bin/cli.js
+++ bin/cli.js
@@ -1,5 +1,18 @@
 #!/usr/bin/env node
-
+const yargs = require('yargs');
 const api = require('../lib/api/api');
 
-api.runServer({port: 3000})
+// configure the options
+const options = yargs.usage('Call tool-runner')
+    .option("r", {describe: 'Default result Path for STEP files.', alias: 'resultPath', type: 'string', demandOption: false})
+    .option("p", {alias: 'port', describe: 'Port to use for the API server', type: 'number', demandOption: false})
+    .option("production", {describe: 'Run the server in production mode.', demandOption: false, type: 'boolean'})
+    .help(true)
+    .argv
+
+// run the server
+api.runServer({
+    port: options.port, 
+    resultPath: options.resultPath, 
+    production: !!options.production
+})
