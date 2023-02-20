198beb7e2275b68edbc60c0683931da13ed01ba8
Mirko Mälicke <mirko@hydrocode.de>
2023-02-20 19:52:36 +0100

    make metadata skippable

--- src/step.ts
+++ src/step.ts
@@ -70,7 +70,9 @@ export const extractFile = (tarPath: string, filename: string, encoding?: Buffer
 
 export interface ShowStepOptions {
     skipErrors?: boolean,
-    skipLog?: boolean
+    skipLog?: boolean,
+    skipMetadata?: boolean,
+    loadFiles?: string[]
 }
 
 export const showStepContent = (path: string, opt: ShowStepOptions ={}): StepContent => {
@@ -83,18 +85,25 @@ export const showStepContent = (path: string, opt: ShowStepOptions ={}): StepCon
 
     // define the callback for handling the content files
     const onentry = (e: tar.ReadEntry) => {
+        // get the input files and output filenames
         if (e.path.includes('/in/') && !e.path.endsWith('/in/')) {
             inputs.push(e.path.slice(e.path.indexOf('/in/') + 4))
         }
         if (e.path.includes('/out/') && !e.path.endsWith('/out/')) {
             outputs.push(e.path.slice(e.path.indexOf('/out/') + 5))
         }
-        if (e.path.endsWith('metadata.json')) {
+
+        // load metadata if not skipped
+        if (e.path.endsWith('metadata.json') && !opt.skipMetadata) {
             metadata = JSON.parse(extractFile(path, e.path, 'utf8') as string)
         }
+
+        // load standard-error stream output if not skipped
         if (e.path.endsWith('STDERR.log') && !opt.skipErrors) {
             errors = extractFile(path, e.path, 'utf8') as string
         }
+
+        // load standard output stream if not skipped
         if (e.path.endsWith('STDOUT.log') && !opt.skipLog) {
             log = extractFile(path, e.path, 'utf8') as string
         }
