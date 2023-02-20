9353e782a72530bfafa34773866bfd2a227888e2
Mirko Mälicke <mirko@hydrocode.de>
2023-02-20 12:33:24 +0100

    add base64 decoding for strings

--- src/parameter.ts
+++ src/parameter.ts
@@ -34,11 +34,26 @@ export const buildParameterFile = (args: any, inDir: string, toolConfig: ToolCon
                     params[paramName] = `/in/${paramName}.csv`
                 }
             } else if (typeof paramValue === 'string') {
+                // this is an existing file ==> copy
                 if (fs.existsSync(paramValue)) {
                     // copy the file and save
                     fs.copyFileSync(paramValue, `${inDir}/${paramName}.${paramValue.split('.').pop()}`)
                     params[paramName] = `/in/${paramName}.${paramValue.split('.').pop()}`
-                } else {
+                } 
+                
+                // this is base64 encoded payload
+                else if (paramValue.includes(';base64,')) {
+                    // split and get the payload
+                    const [mime, payload] = paramValue.substring(5).split(';base64,')
+                    const fname = `${paramName}.${mime.split('/').pop()}`    // here a lookup dict could be implemented
+
+                    // write the file
+                    fs.writeFileSync(`${inDir}/${fname}`, payload, {encoding: 'base64'})
+                    params[paramName] = `/in/${fname}`
+                } 
+                
+                // this is assumed to be a text file
+                else {
                     // plain text file is assumed
                     fs.writeFileSync(`${inDir}/${paramName}.txt`, paramValue)
                     params[paramName] = `/in/${paramName}.txt`
