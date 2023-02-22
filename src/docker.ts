69e3bcff46615b2db6384a643aa7824572d0ec9c
Mirko Mälicke <mirko@hydrocode.de>
2023-02-22 16:27:14 +0100

    add healthz function

--- src/docker.ts
+++ src/docker.ts
@@ -5,4 +5,21 @@ import * as Docker from 'dockerode'
 const docker = new Docker()
 
 
+export interface DockerHealth {
+    running: boolean
+    version: string
+}
+export const healthz = async (): Promise<DockerHealth> => {
+    const version =  await docker.info()
+            .then(value => value.ServerVersion)
+            .catch(() => null)
+
+    return new Promise(resolve => {
+        resolve({
+            running: !!version,
+            version: version ? version : ''
+        })
+    })
+}
+
 export default docker;
